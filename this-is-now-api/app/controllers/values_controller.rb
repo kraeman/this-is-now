59a8b25ed6a93fd9cfe44291f845fb7bfe9f41e1
krae <kraewind@gmail.com>
2021-07-21 17:00:47 -0400

    base value controller

--- this-is-now-api/app/controllers/values_controller.rb
+++ this-is-now-api/app/controllers/values_controller.rb
@@ -1,2 +1,32 @@
 class ValuesController < ApplicationController
+
+    def index
+        values = Value.all
+        render json: ValueSerializer.new(values)
+    end
+
+    def create
+        value = Value.new()
+        value.name = value_params["name"]
+        if value.save 
+          render json: ValueSerializer.new(value)
+        end
+    end
+
+
+      def update
+            value = Value.find(value_params[:id])
+            value.update(value_params)
+            render json ValueSerializer.new(value)
+      end
+
+    def destroy
+        value = Value.find(value_params[:id])
+        value.destroy
+    end
+    
+    private
+        def value_params
+            params.require(:value).permit(:name, :id)
+        end
 end
