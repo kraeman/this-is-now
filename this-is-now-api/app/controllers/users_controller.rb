52067da89d7319c0c2a7229369d559b921f51a55
krae <kraewind@gmail.com>
2021-07-21 16:58:14 -0400

    base user controller

--- this-is-now-api/app/controllers/users_controller.rb
+++ this-is-now-api/app/controllers/users_controller.rb
@@ -1,4 +1,24 @@
 class UsersController < ApplicationController
+
+    def create
+        user = User.new()
+        user.username = user_params["username"]
+        user.password = user_params["password"]
+        if user.save 
+          render json: UserSerializer.new(user)
+        end
+    end
+
+    def update
+        user = User.find(user_params[:id])
+        user.update(user_params)
+        render json UserSerializer.new(user)
+    end
+
     
+    private
+        def user_params
+            params.require(:user).permit(:username, :password, :id)
+        end
 
 end
