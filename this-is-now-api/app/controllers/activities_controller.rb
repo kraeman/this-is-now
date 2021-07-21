06e08ec21791aeeaaf0df5f46646854e11bb4072
krae <kraewind@gmail.com>
2021-07-21 16:54:46 -0400

    base activity controller

--- this-is-now-api/app/controllers/activities_controller.rb
+++ this-is-now-api/app/controllers/activities_controller.rb
@@ -1,2 +1,33 @@
 class ActivitiesController < ApplicationController
+
+    def index
+        activities = Activity.all
+        render json: ActivitySerializer.new(activities)
+    end
+
+    def create
+        activity = Activity.new()
+        activity.name = activity_params["name"]
+        activity.description = activity_params["description"]
+        if activity.save 
+          render json: ActivitySerializer.new(activity)
+        end
+    end
+
+
+      def update
+            activity = Activity.find(activity_params[:id])
+            activity.update(activity_params)
+            render json ActivitySerializer.new(activity)
+      end
+
+    def destroy
+        activity = Activity.find(activity_params[:id])
+        activity.destroy
+    end
+    
+    private
+        def activity_params
+            params.require(:activity).permit(:name, :description, :id)
+        end
 end
