52628de98c1e958b971291b5de23a0f6e4eb2af0
Krae Wind <kraewind@gmail.com>
2021-07-24 22:09:43 -0400

    figured out how to send data wit a get request

--- this-is-now-frontend/src/actions/fetchUser.js
+++ this-is-now-frontend/src/actions/fetchUser.js
@@ -5,7 +5,13 @@ import {loginUser, getReadyToLoginUser} from "./index"
 export function fetchUser(username, password) {
     return (dispatch) => {
       dispatch(getReadyToLoginUser());
-      fetch(`http://localhost:3000/:${username}/:${password}`)
+      fetch('http://localhost:3000/users', {
+        method: 'GET',
+            headers: {
+                "Content-Type": 'application/json'
+            },
+            body: JSON.stringify({username, password})
+      })
         .then(response => response.json())
         .then(user => dispatch(loginUser(user)));
     };
