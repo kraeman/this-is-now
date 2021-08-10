70eb49f3bc38b3d888b187a8877765adbf20bd07
krae <kraewind@gmail.com>
2021-08-10 13:36:07 -0400

    created a lot of values in db

--- this-is-now-api/app/models/activity.rb
+++ this-is-now-api/app/models/activity.rb
@@ -1,5 +1,5 @@
 class Activity < ApplicationRecord
-    has_many :value_activities
+    has_many :value_activities, dependent: :destroy
     has_many :values, through: :value_activities
     has_many :activity_users
     has_many :users, through: :activity_users
--- this-is-now-api/app/models/user.rb
+++ this-is-now-api/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
-    has_many :value_users
+    has_many :value_users, dependent: :destroy
     has_many :values, through: :value_users
     has_many :activity_users
     has_many :activities, through: :activity_users
