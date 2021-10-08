4c3e28c2f0bb5e10db7a45c9789fe32f0c4f7296
krae <kraewind@gmail.com>
2021-10-08 17:13:13 -0400

    REFRESH

--- this-is-now/app/controllers/auth_controller.rb
+++ this-is-now/app/controllers/auth_controller.rb
@@ -1,6 +1,7 @@
 class AuthController < ApplicationController
     skip_before_action :authorized, only: [:create]
 
+    
   def create
     user = User.find_by(username: user_login_params[:username])
     if user && user.authenticate(user_login_params[:password])
